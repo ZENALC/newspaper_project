1216df5df7f8ae49515797a2e2b79b4cfa7a32f0
Mihir Shrestha <mihir.xtha@gmail.com>
2018-12-11 15:24:56 -0500

    updated password change forms

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,18 @@
 # articles/admin.py
 from django.contrib import admin
+from .models import Article, Comment
 
-from .models import Article
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline): # new
+    model = Comment
+    extra = 0  # new
+
+
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin) # new
+admin.site.register(Comment)
