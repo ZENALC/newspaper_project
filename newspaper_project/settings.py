7f6cf9f35b1986f3d932863e020e53fbb4b44fa4
Mihir Shrestha <mihir.xtha@gmail.com>
2018-12-11 13:35:10 -0500

    updated password change forms

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -134,6 +134,10 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # new
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
